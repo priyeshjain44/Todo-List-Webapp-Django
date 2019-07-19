90422b234c1c9487bc9453d1f1a5f1212a39f033
priyeshjain44 <priyeshjain44@gmail.com>
2019-07-19 23:42:44 +0530

    first commit

---
+++ todo_list/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('delete/<list_id>', views.delete, name="delete"),
+    path('uncross/<list_id>', views.uncross, name="uncross"),
+    path('cross_off/<list_id>', views.cross_off, name="cross_off"),
+    path('edit/<list_id>', views.edit, name="edit"),
+]
